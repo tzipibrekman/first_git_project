67d6c3bccf8037e4dd29fd6a4c0787fc29a6bc5c
tova <tovaballoons@gmail.com>
2023-08-07 18:09:59 +0300

    my first commit

--- task6.sh
+++ task6.sh
@@ -1,11 +1,29 @@
 #!/bin/bash
-
+count=$(grep -c '/bin/bash' /etc/passwd)
 users="$(cat /etc/passwd)"
-array=(())
-IFS=':'
-num=0
-for user in $users; do
-	array[num][0]=
-	echo $i
+cols=7
+for ((i=0; i<count; i++)); do
+  for ((j=0; j<cols; j++)); do
+    array[$i,$j]=""
+  done
 done
-
+row=0
+coulmn=0
+for user in $users; do 
+        for detail in $user; do
+            array[row,coulmn]="$detail"
+            ((coulmn++))
+            echo $coulmn
+	   done
+         coulmn=0
+   ((row++))
+done
+echo "${array[0,0]}"
+echo "${array[1,1]}"
+for ((i=0; i<rows; i++)); do
+  for ((j=0; j<cols; j++)); do
+    echo -n "${array[$i,$j]} "
+  done
+  echo 
+done
+echo end of file
