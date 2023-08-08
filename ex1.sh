bd434c5f2abd578e7678059456630b50220adcba
Chen72072 <114311362+Chen72072@users.noreply.github.com>
2023-08-08 14:33:00 +0300

    Create ex1.sh

---
+++ ex1.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+
+
+
+# Check if arguments num is valid
+
+if [ $# -lt 2 ];
+
+then
+
+    echo "num of orgument is not valid"
+
+    exit 1
+
+fi
+
+
+
+directory_path="$1"
+
+
+
+#Check if the directory exists
+
+if [ ! -d "$directory_path" ];
+
+then
+
+    echo "ERROR: directory not exit"
+
+    exit 1
+
+fi
+
+
+
+#Create a dounloads directory
+
+downloads_dir="$directory_path/downloads"
+
+mkdir -p "$downloads_dir"
+
+
+
+shift
+
+
+
+# Loop through the remaining arguments (image URLs)
+for image_url in "$@";
+
+do
+
+        echo "passif"
+
+        # Extract the filename from the URL
+
+        filename=$(basename "$image_url")
+
+
+
+        # Construct the full path for the downloaded image
+
+        full_path="$downloads_dir/$filename"
+
+
+
+        # Download the image using wget
+
+        wget --no-check-certificate -P "$downloads_dir" "$image_url"
+
+
+
+        # Check if the download was successful
+
+        if [ $? -eq 0 ];
+
+        then
+
+            echo "Image downloaded successfully to $full_path"
+
+        else
+
+            echo "Failed to download the image from $image_url"
+
+        fi
+
+done
+
+
+
+#zip folder
+
+
+
+zip -r "$(date +"%Y-%m-%d_%H-%M-%S").zip" "$directory_path"
+
+
+
+
